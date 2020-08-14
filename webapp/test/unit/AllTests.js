8b1d8de19712f676fc5a5858f83f8ee576787ad2
sonip1992 <42675369+sonip1992@users.noreply.github.com>
2020-08-14 13:04:14 +0530

    initial files

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/sap/train/day1/Day_1_Training/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ webapp/test/unit/controller/View1.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"com/sap/train/day1/Day_1_Training/controller/View1.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("View1 Controller");
+
+	QUnit.test("I should test the View1 controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
